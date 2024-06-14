cfa412b47bb0e445b73cc5bdc5518bdac2446f9b
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2024-06-14 10:44:33 +0900

    タスクカードの実装(編集・削除ボタン)

---
+++ src/components/SideMenu/TaskCard/TaskDeleteButton/TaskDeleteButton.tsx
@@ -0,0 +1,19 @@
+import { FaTrashAlt } from "react-icons/fa";
+
+interface TaskDeleteButtonProps {
+    id: string;
+}
+
+const TaskDeleteButton: React.FC<TaskDeleteButtonProps> = ({ id }) => {
+  return (
+    <div>
+      <form action="">
+        <button type='submit' className="hover:text-gray-700 text-lg cursor-pointer">
+            <FaTrashAlt/>
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default TaskDeleteButton
